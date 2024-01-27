4871c89acbbe9c3186f552952575eb26b8a3bc81
David Pardo <davidpardomartin@gmail.com>
2024-01-27 17:33:55 +0100

    basic exercise done withput bonus

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,44 @@
-import React from "react";
+// import React from "react";
+// import SecondsCounter from "./counter";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+// const Home = () => {
+//   let counter = 0;
 
-export default Home;
+//   setInterval(() => {
+//     const six = document.getElementById("six");
+//     const five = document.getElementById("five");
+//     const four = document.getElementById("four");
+//     const three = document.getElementById("three");
+//     const two = document.getElementById("two");
+//     const one = document.getElementById("one");
+
+//     counter += 1;
+
+ 
+//     six.textContent = Math.floor((counter / 100000) % 10);
+//     five.textContent = Math.floor((counter / 10000) % 10);
+//     four.textContent = Math.floor((counter / 1000) % 10);
+//     three.textContent = Math.floor((counter / 100) % 10);
+//     two.textContent = Math.floor((counter / 10) % 10);
+//     one.textContent = counter % 10;
+//   }, 1000);
+
+//   return (
+//     <div className="">
+//       <SecondsCounter />
+//     </div>
+//   );
+// };
+
+
+// const Home = () => {
+// 	  return (
+//     <div className="">
+//       <SecondsCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} digitFive={five} />
+//     </div>
+//   );
+// }
+
+
+// export default Home;
